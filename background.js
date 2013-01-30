702893e90a9c8d604054d9c77372bd9a48d8933e
Faiz <faizamodo@gmail.com>
2013-01-29 20:17:41 -0500

    Fixed the background. Had to declare the setInterval method as a global to do it

--- background.js
+++ background.js
@@ -7,30 +7,24 @@ instructionButtonX = 285;
 buttonY = 415;
 borderRadius = 10;
 buttonHeight = 45;
+go = null;
 
 //life 
 lives = 3;
 
-function drawInstructions(){
-
-}
-
-
-
-
 //animates the move of the background to a different frame
 function moveBackground(frame){
 
+	//Everytime we have to move the background, we stop the previous process.
+	clearInterval(go);
+
+	console.log("background source y: " + background.sy);
 	go = setInterval(function(){
-		console.log(background.sy);
 		var targetHeight = 3600-(frame*600);
 		if (targetHeight < background.sy){
 			background.sy = background.sy - 5;
+		}
+		else if (targetHeight > background.sy){
+			background.sy = background.sy + 5;
 		}}, 5);
-}
-
-//animates the move of the background to a different frame
-function resetBackground(){
-	clearInterval(go);
-	background.sy = 600;
 }
